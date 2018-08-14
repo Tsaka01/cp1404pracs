c48f47a4a38de691d927c76ad9df95a6e9538765
Kate <kate.tsakissiris@my.jcu.edu.au>
2018-08-14 15:46:34 +1000

    Add password check using functions

---
+++ prac_03/password_check_with_functions.py
@@ -0,0 +1,9 @@
+password = input("Enter a password: ")
+if len(password) < 2:
+    print("Invalid - password is too small")
+    password = input("Enter a password: ")
+if len(password) > 10:
+    print("Invalid - password is too large")
+    password = input("Enter a password: ")
+print('*' * len(password))
+print()
