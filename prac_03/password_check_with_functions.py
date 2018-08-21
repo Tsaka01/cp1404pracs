49cdf6498c3c354f48a539423b739ff68252236b
Kate <kate.tsakissiris@my.jcu.edu.au>
2018-08-21 11:11:53 +1000

    Refactor password program to use functions

--- prac_03/password_check_with_functions.py
+++ prac_03/password_check_with_functions.py
@@ -1,9 +1,22 @@
-password = input("Enter a password: ")
-if len(password) < 2:
-    print("Invalid - password is too small")
+def main():
+    password = get_password()
+    show_password(password)
+
+
+def get_password():
     password = input("Enter a password: ")
-if len(password) > 10:
-    print("Invalid - password is too large")
-    password = input("Enter a password: ")
-print('*' * len(password))
-print()
+    if len(password) <= 3:
+        print("Invalid - password is too small")
+        password = input("Enter a password: ")
+    if len(password) > 10:
+        print("Invalid - password is too large")
+        password = input("Enter a password: ")
+    return password
+
+
+def show_password(password):
+    print('*' * len(password))
+    print()
+
+
+main()
