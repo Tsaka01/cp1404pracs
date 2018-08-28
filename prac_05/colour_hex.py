cb4272076b49eb6d7d29a15b25841837fb96fbd8
Kate <kate.tsakissiris@my.jcu.edu.au>
2018-08-28 14:51:56 +1000

    Add colour hex

---
+++ prac_05/colour_hex.py
@@ -0,0 +1,19 @@
+"""
+CP1404 Practical
+Hexadecimal Colour Codes
+"""
+
+HEX_COLOURS = {"Beige": "#f5f5dc", "Brown": "#a52a2a", "Cyan":
+    "#00ffff", "Gray": "#bebebe", "Gold": "#ffd700", "Green":
+    "#00ff00", "Lavender": "#e6e6fa", "Magenta": "ff00ff", "Orange":
+    "#ffa500", "Black": "#000000"}
+
+colour = input("Enter a colour: ")
+while colour != "":
+    print("The code for \"{}\" is {}".format(colour,
+                        HEX_COLOURS.get(colour)))
+    colour = input("Enter a colour: ")
+    # if colour in HEX_COLOURS:
+    # print(colour, "is", HEX_COLOURS[colour])
+    # else:
+    # print("Sorry, this colour isn't available")
